e272466226a80c69977b40de168c2dfed596055b
Anderson Aguiar <andersonlaguiar@gmail.com>
2023-03-19 15:40:38 -0300

    [M3S08] Ex 3 - Números primos

--- main.go
+++ main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+func ePrimo(num int)bool{
+	if num <=1 {
+		return false
+	}
+	if num == 2{
+		return true
+	}
+	if num % 2 == 0{
+		return false
+	}
+	for i := 3; i * i <= num; i++ {
+		if num % i == 0 {
+			return false			
+		}
+	}		
+	return true
+}
+
 func main() {
 	n1 := 3
 	n2 := 3
@@ -14,4 +32,15 @@ func main() {
 	text := "tamanho"
 
 	fmt.Println("O tamanho da string e ", len(text))
+
+	num := 100	
+	
+	for i := 0; i <= num; i++ {
+		numPrimo := ePrimo(i)
+		
+		if numPrimo {
+			fmt.Printf("O numero (%d) é primo", i)	
+			fmt.Println("")					
+		}
+	}
 }
